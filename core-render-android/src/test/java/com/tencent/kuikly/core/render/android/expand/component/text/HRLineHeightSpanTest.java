fc57a0e8fd2c9a39aaab770c596cf322f2f4e7a7
Jiacheng <artin@cat.ms>
2026-07-12 04:36:56 +0800

    fix(android): align line-height with CSS font metrics (#22)
    * fix(android): align line-height with CSS font metrics
    
    Signed-off-by: Raft Android Steward <android-developer-2@mail.build>
    
    * test(android): cover compressed line-height metrics
    
    Signed-off-by: Raft Android Steward <android-developer-2@mail.build>
    
    ---------
    
    Signed-off-by: Raft Android Steward <android-developer-2@mail.build>
    Co-authored-by: Raft Android Steward <android-developer-2@mail.build>

--- core-render-android/src/test/java/com/tencent/kuikly/core/render/android/expand/component/text/HRLineHeightSpanTest.java
+++ core-render-android/src/test/java/com/tencent/kuikly/core/render/android/expand/component/text/HRLineHeightSpanTest.java
@@ -40,10 +40,7 @@ public class HRLineHeightSpanTest {
     }
 
     @Test
-    public void lineHeightDistributesFromLineExtentsWhenFontPaddingDiffers() {
-        // Reverted to top/bottom-based distribution (Slock task #355 audit):
-        // ascent/descent re-basing (7b9503d) and ink-bounds centering
-        // (b992014) both made line placement inconsistent across strings.
+    public void lineHeightDistributesFromCssFontMetricsWhenFontPaddingDiffers() {
         Paint.FontMetricsInt metrics = new Paint.FontMetricsInt();
         metrics.top = -18;
         metrics.ascent = -13;
@@ -52,10 +49,27 @@ public class HRLineHeightSpanTest {
 
         new HRLineHeightSpan(22).chooseHeight("", 0, 0, 0, 0, metrics);
 
-        assertEquals(-17, metrics.top);
-        assertEquals(-17, metrics.ascent);
-        assertEquals(5, metrics.bottom);
-        assertEquals(5, metrics.descent);
+        assertEquals(-15, metrics.top);
+        assertEquals(-15, metrics.ascent);
+        assertEquals(7, metrics.bottom);
+        assertEquals(7, metrics.descent);
         assertEquals(22, metrics.bottom - metrics.top);
+    }
+
+    @Test
+    public void compressedLineHeightStillIgnoresFontPaddingExtents() {
+        Paint.FontMetricsInt metrics = new Paint.FontMetricsInt();
+        metrics.top = -18;
+        metrics.ascent = -13;
+        metrics.descent = 4;
+        metrics.bottom = 6;
+
+        new HRLineHeightSpan(14).chooseHeight("", 0, 0, 0, 0, metrics);
+
+        assertEquals(-12, metrics.top);
+        assertEquals(-12, metrics.ascent);
+        assertEquals(2, metrics.bottom);
+        assertEquals(2, metrics.descent);
+        assertEquals(14, metrics.bottom - metrics.top);
     }
 }
